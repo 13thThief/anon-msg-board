7f4e9a43c19ead493a63478d066c2b1954cc857f
13thThief <manpreet.plus@gmail.com>
2019-09-23 04:37:44 +0530

    test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,28 +9,52 @@ suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     
-    suite('POST', function() {
-      test('POST', function(done) {
+      test('POST', done =>{
       chai.request(server)
           .post('/api/threads/test')
-          .send({text:'Chai test', delete_password:'chai'})
+          .send({text:'test', delete_password:'test'})
           .end(function(err,res){
             assert.equal(res.status, 200);
             done();
           })
         });
-    });
     
-    suite('GET', function() {
-      
-    });
+      test('GET', done =>{
+        chai.request(server)
+          .get('/api/threads/test')
+          .end((err, res)=>{
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.property(res.body[0], '_id');
+            assert.property(res.body[0], 'text');
+            assert.property(res.body[0], 'created_on');
+            assert.property(res.body[0], 'bumped_on');
+            assert.property(res.body[0], 'replies');
+            done();
+          })
+      })
+
+    // PUT first before DELETE
+      test('PUT', done => {
+        chai.request(server)
+            .put('/api/threads/test')
+            .send({thread_id:'5d87f502ccb9710d6ca54896'})
+            .end((err,res) => {
+              assert.equal(res.status, 200);
+              assert.equal(res.text, 'success');
+              done();
+            });
     
-    suite('DELETE', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
+      test('DELETE', done =>{
+        chai.request(server)
+            .delete('/api/threads/test')
+            .send({thread_id:'5d87f502ccb9710d6ca54896', delete_password:'chai'})
+            .end(function(err,res){
+              assert.equal(res.status,200);
+              assert.equal(res.text,'success')
+              done();
+            })
+      });
     });
     
 
@@ -38,20 +62,52 @@ suite('Functional Tests', function() {
   
   suite('API ROUTING FOR /api/replies/:board', function() {
     
-    suite('POST', function() {
-      
+    test('POST', done => {
+      chai.request(server)
+          .post('/api/replies/test')
+          .send({thread_id:'5d87f502ccb9710d6ca54896',text:'reply test',delete_password:'test'})
+          .end(function(err,res){
+            assert.equal(res.status, 200);
+            done();
+          })
     });
     
-    suite('GET', function() {
-      
+    test('GET', done => {
+      chai.request(server)
+          .get('/api/replies/test?thread_id=5d87f502ccb9710d6ca54896')
+          .end(function(err,res){
+            assert.equal(res.status,200);
+            assert.property(res.body,'_id');
+            assert.property(res.body,'text');
+            assert.property(res.body,'created_on');
+            assert.property(res.body,'bumped_on');
+            assert.property(res.body,'replies');
+            assert.isArray(res.body.replies);
+            done();
+          });
     });
     
-    suite('PUT', function() {
-      
+    test('PUT', done =>{
+      chai.request(server)
+          .put('/api/replies/test')
+          .send({thread_id:'5ca9e7a70560730be834eebf',reply_id: '5d87fc88412ad20f58d058f3'})
+          .end(function(err,res){
+            assert.equal(res.status,200);
+            assert.equal(res.text,'success')
+            done();
+          })  
     });
     
-    suite('DELETE', function() {
-      
+    test('DELETE', done => {
+      chai.request(server)
+          .put('/api/replies/test')
+          .send({thread_id:'5d87f502ccb9710d6ca54896',reply_id: '5d87fc88412ad20f58d058f3',delete_password:'test'})
+          .end(function(err,res){
+            assert.equal(res.status,200);
+            console.log(res.body)
+            assert.equal(res.text,'success')
+            done();
+          });
     });
     
   });
